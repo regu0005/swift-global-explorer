64cbbf4b6ab8682ceb13c318b6a585977fdf0471
“regu0005@algonquinlive.com” <regu0005@algonquinlive.com>
2023-11-15 19:19:41 -0500

    Basic structure with dummy data

---
+++ regu0005-mad9137-assignment-2-3/Sections/CountriesListView.swift
@@ -0,0 +1,18 @@
+//
+//  CountriesListView.swift
+//  regu0005-mad9137-assignment-2-3
+//
+//  Created by Gustavo Reguerin on 2023-11-15.
+//
+
+import SwiftUI
+
+struct CountriesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountriesListView()
+}
---
+++ regu0005-mad9137-assignment-2-3/Sections/HeroSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroSectionView.swift
+//  regu0005-mad9137-assignment-2-3
+//
+//  Created by Gustavo Reguerin on 2023-11-15.
+//
+
+import SwiftUI
+
+struct HeroSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroSectionView()
+}
