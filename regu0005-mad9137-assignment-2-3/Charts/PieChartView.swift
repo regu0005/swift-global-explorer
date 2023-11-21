82d6c52893d593ff39d07fd83d89b86f011afd6c
“regu0005@algonquinlive.com” <regu0005@algonquinlive.com>
2023-11-20 22:26:33 -0500

    Languages Chart added and interface improvements

---
+++ regu0005-mad9137-assignment-2-3/Charts/PieChartView.swift
@@ -0,0 +1,8 @@
+//
+//  PieChartView.swift
+//  regu0005-mad9137-assignment-2-3
+//
+//  Created by Gustavo Reguerin on 2023-11-20.
+//
+
+import Foundation
---
+++ regu0005-mad9137-assignment-2-3/Charts/WineType.swift
@@ -0,0 +1,8 @@
+//
+//  WineType.swift
+//  regu0005-mad9137-assignment-2-3
+//
+//  Created by Gustavo Reguerin on 2023-11-20.
+//
+
+import Foundation
